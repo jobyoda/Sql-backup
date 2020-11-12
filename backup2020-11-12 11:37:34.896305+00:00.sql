cce8b9de4dcf94c52076e81abaa5f9f75249843b
jobyoda <74355225+jobyoda@users.noreply.github.com>
2020-11-12 17:07:38 +0530

    File upload

---
+++ backup2020-11-12 11:37:34.896305+00:00.sql
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+ <head>
+  <title>Index of /mysql_backup</title>
+ </head>
+ <body>
+<h1>Index of /mysql_backup</h1>
+  <table>
+   <tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>
+   <tr><th colspan="5"><hr></th></tr>
+<tr><td valign="top"><img src="/icons/back.gif" alt="[PARENTDIR]"></td><td><a href="/">Parent Directory</a></td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="backup-on-2020-11-12-19-33-16.sql">backup-on-2020-11-12-19-33-16.sql</a></td><td align="right">2020-11-12 11:33  </td><td align="right"> 33M</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="backup-on-one-2020-11-12-19-33-20.sql">backup-on-one-2020-11-12-19-33-20.sql</a></td><td align="right">2020-11-12 11:33  </td><td align="right">3.3M</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="backup-on-two-2020-11-12-19-33-29.sql">backup-on-two-2020-11-12-19-33-29.sql</a></td><td align="right">2020-11-12 11:33  </td><td align="right">142M</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="backup-on-two-2020-11-12-19-33-35.sql">backup-on-two-2020-11-12-19-33-35.sql</a></td><td align="right">2020-11-12 11:33  </td><td align="right"> 52M</td><td>&nbsp;</td></tr>
+   <tr><th colspan="5"><hr></th></tr>
+</table>
+<address>Apache/2.4.29 (Ubuntu) Server at 18.191.12.133 Port 80</address>
+</body></html>
